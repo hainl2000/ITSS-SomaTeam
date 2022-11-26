162ef2bf8de11e953b5310e8eb94a1876b4f037a
hainl2000 <haingluong2000@gmail.com>
2022-11-27 01:26:47 +0700

    update template mail

--- api/resources/views/signup.blade.php
+++ api/resources/views/signup.blade.php
@@ -1,4 +1,5 @@
 <div>
     <h1 lang="vi">Welcome to Soma Project </h1>
     <h1 lang="vi">Signup successfully </h1>
+    <h2><a href="https://localhost:3000"> Click here </a> to go to page</h2>
 </div>
